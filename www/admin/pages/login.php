fea0403e44c1e4e46f81b11759fcc0c64112e061
Jiří Dragoun <Jiri.Dragoun@outlook.com>
2019-10-04 16:08:02 +0200

    Minor design changes by: Filip Boháč

--- www/admin/pages/login.php
+++ www/admin/pages/login.php
@@ -4,9 +4,9 @@
             <H1>Přihlášení Admin</H1>
             <hr>
             <label for="name"><b>Email / Jméno</b></label>
-            <input type="text" placeholder="Zadejte Email / Jméno..." name="name" value="" id="name">
+            <input type="text" placeholder="Zadejte Email / Jméno" name="name" value="" id="name">
             <label for="password"><b>Heslo</b></label>
-            <input type="password" placeholder="Zadejte Heslo..." name="password" value="" id="password">
+            <input type="password" placeholder="Zadejte Heslo." name="password" value="" id="password">
             <input type="submit" name="" value="Přihlásit" onclick="login()">
             <img id="load" src="content/img/load.gif" alt="" style="width:200px;height:200px;display:none;margin:0 auto;">
       </div>
