b2e700eb03a4a988c7ab8a46b56d15743ec9d814
Vojtěch Prouza <vojta.prouza.byte@gmail.com>
2019-10-05 17:26:26 +0200

    Script je nyni schopny generovat cast SQL autoru

--- zpracovani.py
+++ zpracovani.py
@@ -9,8 +9,23 @@ autori = list()
 for kniha in knihy:
     #print(kniha)
     kniha = kniha.split(";")
-    print(kniha[1])
+    #print(kniha[1])
     autori.append(kniha[1])
 
-print(autori)
-knihy.close()
+knihy.close() 
+
+autori = list(dict.fromkeys(autori)) #odstraneni duplikatu
+#print(autori)
+
+sql_autori = ""
+for autor in autori:
+    if len(autor.split(" ")) == 2:
+        #print(autor)
+        sql_autori += "('" + autor.split(" ")[0] + "','" + autor.split(" ")[1] + "')\n"
+    elif len(autor.split(" ")) == 3:
+        sql_autori += "('" + autor.split(" ")[0] + " " + autor.split(" ")[1] + "','" + autor.split(" ")[2] + "'),\n"
+    else:
+        sql_autori += "('','" + autor + "'),\n"
+
+
+print(sql_autori)
