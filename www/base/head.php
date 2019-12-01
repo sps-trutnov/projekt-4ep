6c3338ccceb19c545408854b89643873eee8dc28
Jiří Dragoun <Jiri.Dragoun@outlook.com>
2019-12-01 22:30:22 +0100

    more tweeks

--- www/base/head.php
+++ www/base/head.php
@@ -30,20 +30,20 @@
     <a href="./?p=myBooks" class="<?php echo isActive("myBooks") ?> ; hover"><span class="flaticon-bookmark"></span>Moje knihy<div id="notification_bar" class=""><div id="notification_number"></div></div></a>
     
  <!--   <a id="navigace-right" href="./?a=logout" class="<?php # echo isActive("logout") ?>  ; hover"><span class="flaticon-exit"></span>Odhlásit</a> -->
-    
+ <a href=# style='display:none'>
         <?php if($loggedIn){ 
             
 
             echo (' <div class="dropdown" id="horninavigace-log">
-                        <a href="./" class="header_user"><span class="flaticon-user">&nbsp;&nbsp;'.showProfileInfo().'</span></a>
+                        <a href="./" class="header_user"><span class="flaticon-user"></span>'.showProfileInfo().'</a>
                         <div class="dropdown-content">
-                            <a href="./?p=pass_change"><div><span class="flaticon-pass">&nbsp;&nbsp; Změna hesla </div></a><br>
-                            <a href="./?a=logout"><div><span class="flaticon-exit">&nbsp;&nbsp; Odhlásit se</div></a>
+                            <a href="./?p=pass_change"><div><span class="flaticon-pass"></span> Změna hesla </div></a><br>
+                            <a href="./?a=logout"><div><span class="flaticon-exit"></span> Odhlásit se</div></a>
                         </div>
                     </div>');
 
         }?>
-    
+    </a>
     
 
     <a href="javascript:void(0);" class="icon" onclick="sbalit()">&#x2630;</a>
--- www/pages/home.php
+++ www/pages/home.php
@@ -18,5 +18,5 @@
     <a href="./?p=list"><div class="zoom"><img src="./img/flaticon/014-folder.png" alt="seznam_knih"><br>Seznam knih</div></a>
     <a href="./?p=myBooks"><div class="zoom"><img src="./img/flaticon/003-bookmark.png" alt="moje_knihy"><br>Moje knihy</div></a>
     <a href="./?p=qr_reader"><div class="zoom"><img src="./img/flaticon/015-qr-code.png" alt="qr_ctecka"><br>QR čtečka</div></a>
-    
+    <a href="./?a=logout"><div class="zoom"><img src="./img/flaticon/013-exit.png" alt="log_out"><br>Odhlásit se</div></a>
 </ul>
