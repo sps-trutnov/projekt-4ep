571a577194fc7ce948db94b8a57fb6e406af1945
Jan Jörka <jan.jorka@seznam.cz>
2019-12-05 15:02:53 +0100

    Sysadmin přesunut do vlastní složky. Odstraněna práce loňského týmu.

--- www/pages/menu.php
+++
@@ -1,5 +0,0 @@
-<ul id="menu">
-  <li><a href="/admin">Domů</a></li>
-  <li><a href="/admin/?p=show">Výpis uživatelů</a></li>
-  <li><a href="/admin/?p=adduser">Přidat uživatele</a></li>
-</ul>
--- www/php/dbconn.php
+++
@@ -1,1 +0,0 @@
-<?php  $conn = new PDO('mysql:host=localhost;dbname=5ep_vavrinek', 'vavrinek', "Hon999");
