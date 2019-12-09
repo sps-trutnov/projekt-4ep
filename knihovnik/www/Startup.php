88e5ea7387fc9299f4b41225c79f84b4038436f8
Michal Dub <dubmichal1@gmail.com>
2019-12-09 11:22:05 +0100

    Přesun do složky knihovnik

--- knihovnik/www/Startup.php
+++ knihovnik/www/Startup.php
@@ -32,9 +32,9 @@ class Startup {
     
     private function createPDOConnection(): \PDO {
         $hostname = "localhost";
-        $databaseName = "6ep_dymaka";
-        $username = "dymaka";
-        $password = "Lachtan654321";
+        $databaseName = "knihovna";
+        $username = "knihovna";
+        $password = "Heslo96321";
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
