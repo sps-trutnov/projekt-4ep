893a902a094b45afd1c770e94200f8c2d6c11eec
Jan Jörka <jan.jorka@seznam.cz>
2019-12-15 15:55:59 +0100

    Hlavička, titulek a favicon.

--- authentication/signIn.php
+++ authentication/signIn.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-
 require_once "settings.php";
 require_once "User.php";
 
@@ -23,8 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $databaseConnection = createPDOConection();
 
     $invalidCredentials = !signIn($databaseConnection, $userName, $password);
-
-    echo json_encode($invalidCredentials);
 
     if (!$invalidCredentials && isset($_GET["redirectUrl"])) {
         $redirectUrl = $_GET["redirectUrl"];
@@ -87,33 +81,45 @@ function getUserByUserName(PDO $databaseConnection, string $userName): ?User
 ?>
 
 <!DOCTYPE html>
-<html lang="cs">
+<html lang="cs" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="dark.css">
     <link rel="stylesheet" href="signIn.css">
-    <title>Přihlášení</title>
+    <title>Knihovna SPŠ Trutnov</title>
 </head>
-<body>
-    <form class="form form-sign-in p-2" method="post">
-        <h2 class="text-center">Přihlášení</h2>
-        <div class="form-group">
-            <label for="user-name-input">Uživatelské jméno</label>
-            <input class="form-control" id="user-name-input" type="text" name="userName">
-        </div>
-        <div class="form-group">
-            <label for="user-name-input">Heslo</label>
-            <input class="form-control" id="password-input" type="password" name="password">
-            <?php if ($invalidCredentials): ?>
-                <div class="invalid-feedback d-block">
-                    Špatné uživatelské jméno nebo heslo.
-                </div>
-            <?php endif; ?>
-        </div>
-        <button type="submit" class="btn btn-primary w-100">Přihlásit se</button>
-    </form>
+<body class="h-100 d-flex flex-column">
+    <header>
+        <!-- Header placeholder -->
+    </header>
+    <main class="flex-grow-1 d-flex align-items-center justify-content-center">
+        <form class="form form-sign-in p-2" method="post">
+            <h2 class="text-center">Přihlášení</h2>
+            <div class="form-group">
+                <label for="user-name-input">Uživatelské jméno</label>
+                <input class="form-control" id="user-name-input" type="text" name="userName">
+            </div>
+            <div class="form-group">
+                <label for="user-name-input">Heslo</label>
+                <input class="form-control" id="password-input" type="password" name="password">
+                <?php if ($invalidCredentials) : ?>
+                    <div class="invalid-feedback d-block">
+                        Špatné uživatelské jméno nebo heslo.
+                    </div>
+                <?php endif; ?>
+            </div>
+            <button type="submit" class="btn btn-primary w-100">Přihlásit se</button>
+        </form>
+    </main>
+    <footer class="p-1 pr-2 text-muted text-right text-sm">
+        <small>
+            Knihovna
+            <a href="https://www.spstrutnov.cz" target="_blank">SPŠ Trutnov</a>
+        </small>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
