7a49c27655043ed1fddfc432f8180b22fdb70f83
Michal Dub <dubmichal1@gmail.com>
2019-12-19 11:03:31 +0100

    Vyčištění projektu knihovník

--- src/knihovnik/passwordHashing/PasswordHasherInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace passwordHashing;
-
-interface PasswordHasherInterface {
-    public function hash(string $password): string;
-    public function verify(string $password, string $passwordHash): bool;
-}
