2f465f045c886459f0b35d2dfe1c5d8119704dfe
Michal Dub <dubmichal1@gmail.com>
2019-12-19 10:13:42 +0100

    config v rootu

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+const DB_HOST = "localhost";
+const DB_NAME = "knihovna";
+const DB_USER = "knihovna";
+const DB_PASS = "Heslo96321";
+const BASE_URL = "/";
--- knihovnik/www/Startup.php
+++ knihovnik/www/Startup.php
@@ -34,10 +34,10 @@ class Startup {
     }
     
     private function createPDOConnection(): \PDO {
-        $hostname = "localhost";
-        $databaseName = "knihovna";
-        $username = "knihovna";
-        $password = "Heslo96321";
+        $hostname = DB_HOST;
+        $databaseName = DB_NAME;
+        $username = DB_USER;
+        $password = DB_PASS;
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
