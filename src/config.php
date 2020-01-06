1d917058fbaea04cd36fd0bd83118a496cd64f8a
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-01-06 12:58:25 +0100

    config v src je zpátky, ale nevyplněný, config v dist se ignoruje

---
+++ src/config.php
@@ -0,0 +1,17 @@
+<?php
+// Adresa SQL serveru
+const DB_HOST = "";
+// Název databáze
+const DB_NAME = "";
+// Přihlašovací jméno uživatele DB
+const DB_USER = "";
+// Heslo uživatele DB
+const DB_PASS = "";
+
+// URL, kde se aktuálně nachází kořen projektu
+const BASE_URL = "";
+
+// 
+const CORS_ALLOW_ALL_ORIGINS = false;
+// 
+const CORS_ALLOWED_ORIGINS = [];
