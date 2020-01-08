2ca704d38a53c730a869448e7f11b1bd2c2b8528
Michal Dub <dubmichal1@gmail.com>
2020-01-08 21:57:47 +0100

    Konfigurovatelná šířka QR kódu

--- src/config.php
+++ src/config.php
@@ -1,17 +1,20 @@
 <?php
 // Adresa SQL serveru
-const DB_HOST = "localhost";
+const DB_HOST = "";
 // Název databáze
-const DB_NAME = "knihovna";
+const DB_NAME = "";
 // Přihlašovací jméno uživatele DB
-const DB_USER = "knihovna";
+const DB_USER = "";
 // Heslo uživatele DB
-const DB_PASS = "Heslo96321";
+const DB_PASS = "";
 
 // URL, kde se aktuálně nachází kořen projektu
-const BASE_URL = "/src";
+const BASE_URL = "";
 
 // 
 const CORS_ALLOW_ALL_ORIGINS = false;
 // 
 const CORS_ALLOWED_ORIGINS = [];
+
+// Šířka tištěného QR kódu v milimetrech (Výchozí hodnota je 30 mm). Musí být minimálně 18 mm a sudá.
+const QR_WIDTH = 30;
