38b866bc559f42496ae9f9d8a12d266329a8dfb5
Jan Jörka <jan.jorka@seznam.cz>
2020-01-21 22:42:58 +0100

    Po odhlášení ze sysadminu přesměrování na root sysadminu.

--- src/sysadmin/client/src/app/pages/app/main-navigation/main-navigation.component.ts
+++ src/sysadmin/client/src/app/pages/app/main-navigation/main-navigation.component.ts
@@ -1,6 +1,7 @@
 import { Component, InjectionToken, Inject } from '@angular/core';
 import { User } from 'src/app/core/users/user';
 import { SignInService } from 'src/app/core/sign-in/sign-in.service';
+import { Location } from '@angular/common';
 
 export const SIGN_OUT_URL = new InjectionToken<string>("Sign out page url.");
 export const USER_MODULE_URL = new InjectionToken<string>("User module root page url.");
@@ -17,11 +18,14 @@ export class MainNavigationComponent {
     }
 
     constructor(private readonly signInService: SignInService, @Inject(SIGN_OUT_URL) private readonly signOutUrl: string, 
-        @Inject(USER_MODULE_URL) readonly userModuleUrl: string, @Inject(LIBRARIAN_MODULE_URL) readonly librarianModuleUrl: string) {
+        @Inject(USER_MODULE_URL) readonly userModuleUrl: string, @Inject(LIBRARIAN_MODULE_URL) readonly librarianModuleUrl: string, 
+        private readonly location: Location) {
     }
 
     signOut() {
         this.signInService.signOut();
-        window.location.href = this.signOutUrl + "?redirectUrl=" + window.location.href;
+        
+        let rootUrl = this.location.prepareExternalUrl("/");
+        window.location.href = this.signOutUrl + "?redirectUrl=" + rootUrl;
     }
 }
